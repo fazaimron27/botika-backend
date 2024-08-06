7ee774948df8e53d680dbc574361c24673261aa2
fazaimron27 <cincaumyself@gmail.com>
2024-08-06 22:55:09 +0700

    feat: add job model

---
+++ app/Models/Job.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+    use HasFactory;
+
+    protected $table = 'jobs';
+
+    protected $fillable = [
+        'title',
+        'department_id',
+    ];
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    public function employees()
+    {
+        return $this->hasMany(Employee::class);
+    }
+}
